e84bda8995d39abb59b8973751c0c7120c033dfd
Isaac Ackerman <ackeri@dunbar.cs.washington.edu>
2014-06-25 15:16:09 -0700

    Temp changes to User Management Module

--- dalvik/src/main/java/dalvik/agate/UserManagementModule.java
+++ dalvik/src/main/java/dalvik/agate/UserManagementModule.java
@@ -19,7 +19,21 @@ public class UserManagementModule {
         return userId;
     }
 
-    native public static int loginImpl(String user, String password);
-    native public static int adddGroup(String group);
-    native public static int addUserToGroup(String user, String group);
+    public static int addGroup(String groupName) {
+	if(userId < 0)
+	    return -1;
+	return addGroup(groupName, userId);
+    }
+
+    public static int addUserToGroup(String user, String group) {
+	if(userId < 0)
+	    return -1;
+	return addUserToGroup(user, group, userId);
+    }
+
+    native private static int loginImpl(String user, String password);
+    //return -1 on failure, 0 on success
+    native public static int addUser(String user, String password);
+    native private static int addGroup(String group, int ownerId);
+    native private static int addUserToGroup(String user, String group, int userId);
 }
