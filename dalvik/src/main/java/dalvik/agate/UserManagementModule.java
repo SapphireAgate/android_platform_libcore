6458fe3aed736756d552dd8f28f7e43b2f832d37
Isaac Ackerman <ackeri@dunbar.cs.washington.edu>
2014-06-25 17:17:49 -0700

    moved functionality to backing native library:

--- dalvik/src/main/java/dalvik/agate/UserManagementModule.java
+++ dalvik/src/main/java/dalvik/agate/UserManagementModule.java
@@ -7,33 +7,11 @@ package dalvik.agate;
  */
 public class UserManagementModule {
 
-    // assume this is tainted(can't flow anywhere outside this address space)
-    private static int userId = -1;
+    //native public static String getUserName();
 
-    public static int login(String user, String password) {
-        userId = loginImpl(user, password);
-        return userId;
-    }
-
-    public static int getUserId() {
-        return userId;
-    }
-
-    public static int addGroup(String groupName) {
-	if(userId < 0)
-	    return -1;
-	return addGroup(groupName, userId);
-    }
-
-    public static int addUserToGroup(String user, String group) {
-	if(userId < 0)
-	    return -1;
-	return addUserToGroup(user, group, userId);
-    }
-
-    native private static int loginImpl(String user, String password);
     //return -1 on failure, 0 on success
+    native public static int login(String user, String password);
     native public static int addUser(String user, String password);
-    native private static int addGroup(String group, int ownerId);
-    native private static int addUserToGroup(String user, String group, int userId);
+    native public static int addGroup(String group);
+    native public static int addUserToGroup(String user, String group);
 }
